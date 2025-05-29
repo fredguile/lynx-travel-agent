5c912cb52b040a89b98a9b4367bcdc5ce125e57f
Fred G <fredguile@gmail.com>
2025-05-29 11:34:13 +0200

    Create dev and prod tables

--- supabase/schemas/without_fts/semantic_search.sql
+++ supabase/schemas/without_fts/semantic_search.sql
@@ -1,6 +1,6 @@
-drop function if exists semantic_search(vector,integer,jsonb);
+drop function if exists panpac.semantic_search_dev(vector,integer,jsonb);
 
-create or replace function semantic_search(
+create or replace function panpac.semantic_search_dev(
   query_embedding vector(1536),
   match_count int DEFAULT null,
   filter jsonb DEFAULT '{}'::jsonb
@@ -20,10 +20,40 @@ begin
     id,
     content,
     metadata,
-    1 - (emails.embedding <=> query_embedding) as similarity
-  from emails
+    1 - (panpac.emails_dev.embedding <=> query_embedding) as similarity
+  from panpac.emails_dev
   where metadata @> filter
-  order by emails.embedding <=> query_embedding
+  order by panpac.emails_dev.embedding <=> query_embedding
+  limit match_count;
+end;
+$$;
+
+drop function if exists panpac.semantic_search_prod(vector,integer,jsonb);
+
+create or replace function panpac.semantic_search_prod(
+  query_embedding vector(1536),
+  match_count int DEFAULT null,
+  filter jsonb DEFAULT '{}'::jsonb
+) 
+returns table (
+  id bigint,
+  content text,
+  metadata jsonb,
+  similarity float
+)
+language plpgsql
+as $$
+#variable_conflict use_column
+begin
+  return query
+  select
+    id,
+    content,
+    metadata,
+    1 - (panpac.emails_prod.embedding <=> query_embedding) as similarity
+  from panpac.emails_prod
+  where metadata @> filter
+  order by panpac.emails_prod.embedding <=> query_embedding
   limit match_count;
 end;
 $$;
